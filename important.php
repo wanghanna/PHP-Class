72e5285ae93afe7cfce8c6f3bdc4919a9b3ca9e3
wanghanna <hannawang0306@livecom>
2019-11-25 14:12:55 -0800

    upload

---
+++ important.php
@@ -0,0 +1,21 @@
+<?php
+echo "<h1>輸出方式</h1>";
+print "echo / print / print_r() / var_dump()";
+?>
+
+<?php
+echo "<h1>方程式</h1>";
+print "+ ; - ; * ; /<br>+= ; -= ; ++ ; --<br>.="
+?>
+
+<?php
+echo "<h1>判斷式</h1>";
+echo "if(條件式){}<br>
+elseif(條件式){}<br>
+else{}";
+?>
+
+<?php
+echo "<h1>是否存在</h1>";
+print "isset() ; empty() ; is_null()";
+?>
