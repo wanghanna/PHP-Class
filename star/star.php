a009f8ea2b53c50d9b82b149fe0afa47feb7daf3
wanghanna <hannawang0306@livecom>
2019-11-26 20:28:19 -0800

    旋轉的星星

---
+++ star/star.php
@@ -0,0 +1,25 @@
+<style>
+@keyframes go{
+    from{
+transform:rotate(0deg);
+    }
+    to{
+transform:rotate(360deg);
+    }
+}
+</style>
+<body style="width:100vw;height:100vh;background:black">
+ <?php
+ $starNum=rand(50,200);
+for($i=1;$i<=$starNum;$i++){
+$poT=rand(0,100);
+$poL=rand(0,100);
+$size=rand(20,80);
+$colorSetR=rand(100,255);
+ $colorSetG=rand(50,255);
+ $colorSetB=rand(200,255);
+ $time=rand(10,100)/10;
+echo "<span style='color:white;position:absolute;top:".$poT."vh;left:".$poL."vw;font-size:".$size."px;color:rgb(".$colorSetR.",".$colorSetG.",".$colorSetB.");animation:go ".$time."s linear infinite;'>★</span>";
+}
+?>   
+</body>
